8965c8f3eeeb4ed97a5dff3359f25df4db87c52d
MathieuAudibert <mathieu.audibert27@gmail.com>
2024-07-17 23:42:13 +0200

    2 commits is all that is required to make things work �

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # List python adresses 
-![Static Badge](https://img.shields.io/badge/On%20Date-False-red?style=plastic&logoColor=blue)
+![Static Badge](https://img.shields.io/badge/On%20Date-True-green?style=plastic&logoColor=blue)
 
+A bit slow to run the script, idk why
+
+ Check the /src/results/fake/ips.json file 😎
--- src/main.py
+++ src/main.py
@@ -1,4 +1,35 @@
 import ipaddress as ip
+import subprocess
+import os 
 import json
+from dotenv import load_dotenv
 
-network = ip
+def ping_ip(ip):
+    """
+    hihiih on ping les ip hihihihi
+    """
+    try: 
+        output = subprocess.check_output(f"ping -n 1 -w 1 {ip}", stderr=subprocess.STDOUT, universal_newlines=True)
+        return "TTL=" in output
+    
+    except subprocess.CalledProcessError:
+        return False
+
+def scan_network(network):
+    return [str(ip) for ip in ip.IPv4Network(network) if ping_ip(str(ip))]
+
+def main():
+    load_dotenv()
+    network = os.getenv("NETWORK_RANGE")
+    active_ips = scan_network(network)
+
+    chemin = "results/real"
+    if not os.path.exists(chemin):
+        os.makedirs(chemin)
+    with open(os.path.join(chemin, "ips.json"), "w") as f:
+        json.dump(active_ips, f)
+
+    print("Scan terminé")
+
+if __name__ == "__main__":  
+    main()
